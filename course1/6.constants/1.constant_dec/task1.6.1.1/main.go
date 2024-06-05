7e08d1ae5929b030b7cc61b28e48ebe9c59838e1
Evgenii Burdin <e.burdin@automacon.ru>
2024-06-05 11:26:28 +0300

    6.constants

---
+++ course1/6.constants/1.constant_dec/task1.6.1.1/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+const Monday string = "Понедельник"
+const Tuesday string = "Вторник"
+const Wednesday string = "Среда"
+const Thursday string = "Четверг"
+const Friday string = "Пятница"
+const Saturday string = "Суббота"
+const Sunday string = "Воскресенье"
+
+func main() {
+	fmt.Println(Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday)
+}
---
+++ course1/6.constants/1.constant_dec/task1.6.1.2/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+const Monday int = 1
+const Tuesday int = 2
+const Wednesday int = 3
+const Thursday int = 4
+const Friday int = 5
+const Saturday int = 6
+const Sunday int = 7
+
+func main() {
+	fmt.Println(Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday)
+}
